aaaa8189dbcf2646245f101eb1e04de0f5605dc8
MemoVault <max.galkow@gmail.com>
2025-06-20 23:27:58 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>MemoVault</title>
+      </Head>
+      <main>
+        <h1>Welcome to MemoVault</h1>
+      </main>
+    </div>
+  );
+}
